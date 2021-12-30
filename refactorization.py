b6e61493ca38a42bbf4044bd1299fc831847954c
Lucasmirandac <lucasdemirandacosta@gmail.com>
2021-12-29 23:54:31 -0300

    dealing with shots still have bugs

--- refactorization.py
+++ refactorization.py
@@ -1,6 +1,7 @@
 import time
 import threading
 import curses
+from random import randint
 
 mutex = threading.Lock()
 
@@ -45,11 +46,12 @@ class Player (threading.Thread):
             pass
 
         if event == self.left:
-            mutex.acquire()  
+            mutex.acquire()
             # move jogador para esquerda
             coordinates = [self.position[0], self.position[1] - 1]
-            if coordinates[1] !=1:
-                win.addch(self.position[0], self.position[1], ' ') # apaga posicao atual
+            if coordinates[1] != 1:
+                # apaga posicao atual
+                win.addch(self.position[0], self.position[1], ' ')
                 self.position[1] = coordinates[1]
                 self.position[0] = coordinates[0]
                 win.addch(coordinates[0], coordinates[1], "x")
@@ -57,7 +59,7 @@ class Player (threading.Thread):
 
         if event == self.shot:
             mutex.acquire()
-            self.shotPosition[0] = self.position[0] + 1
+            self.shotPosition[0] = self.position[0] - 1
             self.shotPosition[1] = self.position[1]
             win.addch(self.shotPosition[0], self.shotPosition[1], ".")
             mutex.release()
@@ -69,28 +71,56 @@ class Player (threading.Thread):
                 self.shotPosition[0] = self.shotPosition[0] - 1
                 win.addch(old_position, self.shotPosition[1], " ")
             mutex.release()
+
         time.sleep(0.02)
 
     def controlMovements(self, event):
         threading.Thread(target=self._controlMovements, args=(event,)).start()
+
+
 class Enemie (threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.position = [4, 28]
-        self.shotPosition = [0, 0]
-        win.addch(self.position[0], self.position[1], 'o')
+        self.enemie_position = [4, 28]
+        self.enemie_shot_position = [0, 0]
+        win.addch(self.enemie_position[0], self.enemie_position[1], 'o')
+        self.operator = 1
+        self.direction = 'right'
 
     def _enemie_movement(self):
-        win.addch(self.position[0], self.position[1], ' ')
-        for x in range(WINDOW_WIDTH/2):
+
+        for x in range(4):
+            if self.direction == 'right':
+                erase = self.enemie_position[1] - 5
+            else:
+                erase = self.enemie_position[1] + 5
+
+            if self.enemie_position[1] == WINDOW_WIDTH-6:
+                self.operator = -1
+                self.direction = 'left'
+
+            if self.enemie_position[1] == 6:
+                self.operator = 1
+                self.direction = 'right'
+
             mutex.acquire()
-            self.position = self.position+1
-            win.addch(self.position[0], self.position[1], 'x')
+            self.enemie_position[1] = self.enemie_position[1] + self.operator
             mutex.release()
+            win.addch(self.enemie_position[0], self.enemie_position[1], 'o')
+            win.addch(self.enemie_position[0], erase, ' ')
+            self.enemie_shot_position[0] = self.enemie_position[0] + 1
+            self.shot()
+
+    def shot(self):
+        time.sleep(randint(10, 100))
+        shot_in_the_middle = self.enemie_position[1] - 2
+        win.addch(self.enemie_shot_position[0], shot_in_the_middle, '|')
+        win.addch(self.enemie_position[0], shot_in_the_middle, ' ')
+
         time.sleep(0.02)
 
     def enemie_movement(self):
-        threading.Thread(target=self._enemie_movement, args=(event,)).start()
+        threading.Thread(target=self._enemie_movement).start()
 
 
 def updateScreen():
@@ -98,7 +128,6 @@ def updateScreen():
     win.timeout(150)
     mutex.release()
     time.sleep(0.01)
-
 
 curses.initscr()
 win = curses.newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 0, 0)
