3b144f9de5948b85fdedae57253f05b8a74641b0
Lucasmirandac <lucasdemirandacosta@gmail.com>
2022-01-04 08:32:20 -0300

    refactoring

--- refactorization.py
+++ refactorization.py
@@ -2,16 +2,20 @@ import time
 import threading
 import curses
 from random import randint
-
+sem = threading.Semaphore()
 mutex = threading.Lock()
-
-key = curses.KEY_DOWN
-score = 0
 
 WINDOW_WIDTH = 60  # number of columns of window box
 WINDOW_HEIGHT = 20  # number of rows of window box
 ESC = 27
-
+score = 0
+curses.initscr()
+win = curses.newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 0, 0)
+win.keypad(1)
+curses.noecho()
+curses.curs_set(0)
+win.border(0)
+win.nodelay(1)
 
 class Player (threading.Thread):
     def __init__(self):
@@ -20,27 +24,20 @@ class Player (threading.Thread):
         self.right = curses.KEY_RIGHT
         self.left = curses.KEY_LEFT
         self.shot = curses.KEY_UP
-        self.event = win.getch()
         self.position = [18, 30]
-        self.shotPosition = [0, 0]
+        self.shotPosition = [0, 0]        
 
-    def begin(self):
-        win.addch(self.position[0], self.position[1],
-                  'x')  # Colocar no init do Player
-
-    def _controlMovements(self, event):
+    def controlMovements(self, event):
         if event == self.right:
-            mutex.acquire()
+            sem.acquire()
             coordinates = [self.position[0], self.position[1] + 1]
-
             if coordinates[1] != WINDOW_WIDTH-1:
                 # apaga posicao atual
                 win.addch(self.position[0], self.position[1], ' ')
                 self.position[1] = coordinates[1]
                 self.position[0] = coordinates[0]
                 win.addch(coordinates[0], coordinates[1], "x")
-
-            mutex.release()
+            sem.release()
 
         if self.position == WINDOW_WIDTH - 1:
             pass
@@ -64,30 +61,27 @@ class Player (threading.Thread):
             win.addch(self.shotPosition[0], self.shotPosition[1], ".")
             mutex.release()
 
-            mutex.acquire()
             for x in range(WINDOW_HEIGHT-4):
                 win.addch(self.shotPosition[0], self.shotPosition[1], ".")
                 old_position = self.shotPosition[0]
                 self.shotPosition[0] = self.shotPosition[0] - 1
                 win.addch(old_position, self.shotPosition[1], " ")
-            mutex.release()
 
         time.sleep(0.02)
-
-    def controlMovements(self, event):
-        threading.Thread(target=self._controlMovements, args=(event,)).start()
 
 
 class Enemie (threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        self.enemie_position = [4, 28]
-        self.enemie_shot_position = [0, 0]
-        win.addch(self.enemie_position[0], self.enemie_position[1], 'o')
-        self.operator = 1
-        self.direction = 'right'
+        self.enemie_position = [4, 28]  # posição do inimigo
+        self.enemie_shot_position = [0, 0]  # direção do tiro
+        self.operator = 1 ## define esquerda ou direita, está começando para direita
+        self.direction = 'right' 
 
-    def _enemie_movement(self):
+    def begin(self):
+        win.addch(self.enemie_position[0], self.enemie_position[1], 'o')
+
+    def enemie_movement(self):
 
         for x in range(4):
             if self.direction == 'right':
@@ -109,50 +103,44 @@ class Enemie (threading.Thread):
             win.addch(self.enemie_position[0], self.enemie_position[1], 'o')
             win.addch(self.enemie_position[0], erase, ' ')
             self.enemie_shot_position[0] = self.enemie_position[0] + 1
-            self.shot()
+            #self.shot()
 
     def shot(self):
-        time.sleep(randint(10, 100))
         shot_in_the_middle = self.enemie_position[1] - 2
         win.addch(self.enemie_shot_position[0], shot_in_the_middle, '|')
         win.addch(self.enemie_position[0], shot_in_the_middle, ' ')
 
         time.sleep(0.02)
 
-    def enemie_movement(self):
-        threading.Thread(target=self._enemie_movement).start()
-
 
 def updateScreen():
     mutex.acquire()
-    win.timeout(150)
+    win.timeout(100)
     mutex.release()
     time.sleep(0.01)
 
-curses.initscr()
-win = curses.newwin(WINDOW_HEIGHT, WINDOW_WIDTH, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
+def main():
+    enemie = Enemie()
+    enemie.begin()
+    thread3 = threading.Thread(target=updateScreen)
+    thread3.start()
+    win.addch(18, 30, 'x')  # inicializando posição do jogador
+    key = curses.KEY_DOWN
+    player = Player()
+    enemie = Enemie()
+    player.start()
+    enemie.start()
+    while key != ESC:
+        event = win.getch()
+        if event != -1:
+            key = event
+        player.controlMovements(event)
+        enemie.enemie_movement()
 
-player = Player()
-player.begin()
-enemie = Enemie()
+    thread3.join()
+    player.join()
 
-while key != ESC:
-    event = win.getch()
-    if event != -1:
-        key = event
-    thread1 = player.controlMovements(event)
-    thread3 = threading.Thread(target=updateScreen).start()
-    thread2 = enemie.enemie_movement()
+    curses.endwin()
+    print(f"Final score = {score}")
 
-thread3.join()
-thread1.join()
-thread2.join()
-
-
-curses.endwin()
-print(f"Final score = {score}")
+main()
